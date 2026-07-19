d3c79d5c565a49ae22c9bcd6053a916a07e5f441
kushaan183-dot <kushaan183@gmail.com>
2026-07-19 08:45:48 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
-        int x = 5;
-        int y = 10; 
-        System.out.println(x + y);
-        System.out.println("The difference of y and x is: " + (y - x) + "where x=5 and y=10");
+        int x = 10;
+        int y = 15;
+        int z = x + y;
+        System.out.println("The sum of x and y is: " + z + " where x=" + x + " and y=" + y + ".");
     }
 }
