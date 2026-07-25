df2d57e77c7d310be5645b669e9f5fa086eafbc2
kushaan183-dot <kushaan183@gmail.com>
2026-07-25 11:35:15 +0530

    Add files via upload

---
+++ Java_loops.java
@@ -0,0 +1,14 @@
+class Java_loops {
+    public static void main(String[] args) {
+        // for loop
+        for(int i=10;i>1;i--){
+            System.out.println("the value of i is:"+i);
+        }
+        // using while loop
+        int j=0;
+        while(j<11){
+            System.out.println("the value of j is:"+j);
+            j++;
+        }
+    }
+}
