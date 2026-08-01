fcdd4f1fe7c68acf7a309c7ce044884bb981e632
kushaan183-dot <kushaan183@gmail.com>
2026-08-01 11:56:36 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,11 @@
+class age {
+    int age;
+    age(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        age obj = new age(24);
+        System.out.println("obj.age: " + obj.age);
+    }
+}
