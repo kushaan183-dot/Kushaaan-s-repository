ae81a9ab55dd3f418321eda21f769619865f8803
kushaan183-dot <kushaan183@gmail.com>
2026-09-10 19:06:54 +0530

    Add files via upload

---
+++ deep_learning.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras import layers,models
+import matplotlib.pyplot as plt
+(x_train,y_train), (x_test,y_test)=tf.keras.datasets.mnist.load_data()
+x_train,x_test=x_train/255.0, x_test/255/0
+model=models.Sequential([
+    layers.Flatten(input_shape=(28,28)),
+    layers.Dense(128,activation='relu'),
+    layers.Dense(10,activation='softmax')    
+])
+model.compile(optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=['accuracy'])
+model.fit(x_train,y_train,epochs=5)
+test_loss,test_acc=model.evaluate(x_test,y_test)
+print(f"The test accuracy is:{test_acc}")
+predictions=model.predict(x_test)
+plt.imshow(x_test[1],cmap=plt.cm.binary)
+plt.title(f"Predicted:{predictions[1].argmax()}")
+plt.show()
